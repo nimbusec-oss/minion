9c88e7473c7ccde92544602059c958f2e78095b3
Thomas Kastner <tom@sprungknoedl.at>
2017-03-17 12:12:46 +0100

    changed to more explicit session handling

--- minion.go
+++ minion.go
@@ -28,24 +28,6 @@ const (
 	RedirectKey = "_redirect"
 )
 
-// ErrorFormat defines as which content type an error should be serialized
-type ErrorFormat string
-
-const (
-	// ErrorAsHTML formats an error using the HTML template `error.html`.
-	ErrorAsHTML ErrorFormat = "html"
-	// ErrorAsJSON formats the error as JSON object.
-	ErrorAsJSON ErrorFormat = "json"
-)
-
-const (
-	// TemplatePrincipalKey is the name templates can use to access the principal
-	TemplatePrincipalKey = "principal"
-
-	// TemplateFlashesKey is the name templates can use to access flash messages
-	TemplateFlashesKey = "flashes"
-)
-
 // Logger is a simple interface to describe something that can write log output.
 type Logger interface {
 	Printf(fmt string, v ...interface{})
@@ -75,18 +57,18 @@ type Minion struct {
 
 	// Error is called for any error that occur during the request processing, be
 	// it client side errors (4xx status) or server side errors (5xx status). The
-	// default handler wirr execute the HTML template `ErrorTemplate`.
+	// default handler will execute the HTML template `ErrorTemplate`.
 	Error         func(w http.ResponseWriter, r *http.Request, code int, err error)
 	ErrorTemplate string
 
-	SessionStore sessions.Store
-	SessionName  string
+	Sessions    sessions.Store
+	SessionName string
 
 	Templates *template.Template
 }
 
 // NewMinion creates a new minion instance.
-func NewMinion(options ...Option) Minion {
+func NewMinion(options ...Option) *Minion {
 	m := &Minion{
 		Debug:  os.Getenv("DEBUG") == "true",
 		Logger: log.New(os.Stderr, "", log.LstdFlags),
@@ -94,6 +76,8 @@ func NewMinion(options ...Option) Minion {
 		UnauthorizedURL:   "/login",
 		ErrorTemplate:     "500.html",
 		ForbiddenTemplate: "403.html",
+
+		Sessions: NilStore{},
 	}
 
 	// default handlers
@@ -106,159 +90,7 @@ func NewMinion(options ...Option) Minion {
 		m = option(m)
 	}
 
-	return *m
-}
-
-// Session can be used in the NewMinion function to add an secure cookie based session.
-func Session(name string, key []byte, options *sessions.Options) Option {
-	return func(m *Minion) *Minion {
-		store := sessions.NewCookieStore(key)
-		store.Options = options
-
-		m.SessionStore = store
-		m.SessionName = name
-		return m
-	}
-}
-
-func (m Minion) openSession(w http.ResponseWriter, r *http.Request) (*sessions.Session, error) {
-	if m.SessionStore == nil {
-		return nil, errors.New("no session store configured")
-	}
-
-	session, err := m.SessionStore.Get(r, m.SessionName)
-	if err != nil {
-		m.Logger.Printf("[warning] failed to open session, created new: %v", err)
-	}
-
-	return session, nil
-}
-
-// Get retrieves a value from the active session. If the value does not
-// exist in the session, a provided default is returned
-func (m Minion) Get(w http.ResponseWriter, r *http.Request, name string, def interface{}) interface{} {
-	session, err := m.openSession(w, r)
-	if err != nil {
-		return def
-	}
-	value, ok := session.Values[name]
-	if !ok {
-		return def
-	}
-	return value
-}
-
-// Set stores a value in the active session.
-func (m Minion) Set(w http.ResponseWriter, r *http.Request, name string, value interface{}) {
-	session, err := m.openSession(w, r)
-	if err != nil {
-		return
-	}
-
-	session.Values[name] = value
-}
-
-// Delete removes a value from the active session.
-func (m Minion) Delete(w http.ResponseWriter, r *http.Request, name string) {
-	session, err := m.openSession(w, r)
-	if err != nil {
-		return
-	}
-
-	delete(session.Values, name)
-}
-
-// ClearSession removes all values from the active session.
-func (m Minion) ClearSession(w http.ResponseWriter, r *http.Request) {
-	session, err := m.openSession(w, r)
-	if err != nil {
-		return
-	}
-
-	session.Options.MaxAge = -1
-	session.Save(r, w)
-}
-
-// AddFlash adds a flash message to the session.
-func (m Minion) AddFlash(w http.ResponseWriter, r *http.Request, value interface{}) {
-	session, err := m.openSession(w, r)
-	if err != nil {
-		return
-	}
-
-	session.AddFlash(value)
-}
-
-// Flashes returns a slice of flash messages from the session.
-func (m Minion) Flashes(w http.ResponseWriter, r *http.Request) []interface{} {
-	session, err := m.openSession(w, r)
-	if err != nil {
-		return nil
-	}
-
-	flashes := session.Flashes()
-	return flashes
-}
-
-func (m Minion) SaveSession(w http.ResponseWriter, r *http.Request) {
-	if m.SessionStore == nil {
-		// bail out if no session store is configured
-		return
-	}
-
-	session, err := m.openSession(w, r)
-	if err != nil {
-		m.Logger.Printf("session open: %v", err)
-		return
-	}
-
-	err = session.Save(r, w)
-	if err != nil {
-		m.Logger.Printf("session save: %v", err)
-		return
-	}
-}
-
-// Secured requires that the user has at least one of the provided roles before
-// the request is forwarded to the secured handler.
-func (m Minion) Secured(fn http.HandlerFunc, roles ...string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		principal := m.Get(w, r, PrincipalKey, Anonymous{}).(Principal)
-		if !principal.Authenticated() {
-			m.Unauthorized(w, r)
-			return
-		}
-
-		if !principal.HasAnyRole(roles...) {
-			m.Forbidden(w, r)
-			return
-		}
-
-		fn(w, r)
-	}
-}
-
-// defaultUnauthorizedHandler is the default handler for minion.Unauthorized
-func (m *Minion) defaultUnauthorizedHandler(w http.ResponseWriter, r *http.Request) {
-	session, err := m.openSession(w, r)
-	if err != nil {
-		m.Error(w, r, http.StatusBadRequest, err)
-		return
-	}
-
-	session.Values[RedirectKey] = r.URL.String()
-	err = session.Save(r, w)
-	if err != nil {
-		m.Error(w, r, http.StatusInternalServerError, err)
-		return
-	}
-
-	http.Redirect(w, r, m.UnauthorizedURL, http.StatusSeeOther)
-}
-
-// defaultForbiddenHandler is the default handler for minion.Forbidden
-func (m *Minion) defaultForbiddenHandler(w http.ResponseWriter, r *http.Request) {
-	m.HTML(w, r, http.StatusForbidden, m.ForbiddenTemplate, V{})
+	return m
 }
 
 // defaultErrorHandler is the default handler for minion.Error
@@ -271,64 +103,22 @@ func (m *Minion) defaultErrorHandler(w http.ResponseWriter, r *http.Request, cod
 }
 
 // JSON outputs the data encoded as JSON.
-func (m Minion) JSON(w http.ResponseWriter, r *http.Request, code int, data interface{}) {
-	m.SaveSession(w, r)
+func (m *Minion) JSON(w http.ResponseWriter, r *http.Request, code int, data interface{}) {
 	w.Header().Add("content-type", "application/json; charset=utf-8")
 	w.WriteHeader(code)
 
 	err := json.NewEncoder(w).Encode(data)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintf(w, "failed to encode json: %v", err)
 		m.Logger.Printf("failed to encode json: %v", err)
 	}
 }
 
-// HTML outputs a rendered HTML template to the client. This function also includes
-// some default variables into the template scope.
+// HTML outputs a rendered HTML template to the client.
 func (m *Minion) HTML(w http.ResponseWriter, r *http.Request, code int, name string, data V) {
 	// reload templates in debug mode
 	if m.Templates == nil || m.Debug {
-		fm := template.FuncMap{
-			"div": func(dividend, divisor int) float64 {
-				return float64(dividend) / float64(divisor)
-			},
-			"json": func(v interface{}) template.JS {
-				b, _ := json.MarshalIndent(v, "", "  ")
-				return template.JS(b)
-			},
-			"dict": func(values ...interface{}) (map[string]interface{}, error) {
-				if len(values)%2 != 0 {
-					return nil, errors.New("invalid dict call")
-				}
-				dict := make(map[string]interface{}, len(values)/2)
-				for i := 0; i < len(values); i += 2 {
-					key, ok := values[i].(string)
-					if !ok {
-						return nil, errors.New("dict keys must be strings")
-					}
-					dict[key] = values[i+1]
-				}
-				return dict, nil
-			},
-		}
-
-		m.Templates = template.New("").Funcs(fm)
-		err := filepath.Walk("./templates", func(path string, info os.FileInfo, err error) error {
-			if !info.IsDir() {
-				b, err := ioutil.ReadFile(path)
-				if err != nil {
-					return err
-				}
-
-				m.Templates, err = m.Templates.
-					New(strings.TrimPrefix(path, "templates/")).
-					Parse(string(b))
-				return err
-			}
-			return nil
-		})
-
+		err := m.LoadTemplates()
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			fmt.Fprintf(w, "failed to parse templates: %v", err)
@@ -337,48 +127,60 @@ func (m *Minion) HTML(w http.ResponseWriter, r *http.Request, code int, name str
 		}
 	}
 
-	flashes := m.Flashes(w, r)
-	data[TemplateFlashesKey] = flashes
-
-	principal := m.Get(w, r, PrincipalKey, Anonymous{}).(Principal)
-	data[TemplatePrincipalKey] = principal
-
-	m.SaveSession(w, r)
 	w.Header().Add("content-type", "text/html; charset=utf-8")
 	w.WriteHeader(code)
 
 	err := m.Templates.ExecuteTemplate(w, name, data)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintf(w, "failed to execute template %q: %v", name, err)
 		m.Logger.Printf("failed to execute template %q: %v", name, err)
 		return
 	}
 }
 
-// Principal is an entity that can be authenticated and verified.
-type Principal interface {
-	Authenticated() bool
-	ID() string
-	HasAnyRole(roles ...string) bool
+func (m *Minion) LoadTemplates() error {
+	fm := template.FuncMap{
+		"div": func(dividend, divisor int) float64 {
+			return float64(dividend) / float64(divisor)
+		},
+		"json": func(v interface{}) template.JS {
+			b, _ := json.MarshalIndent(v, "", "  ")
+			return template.JS(b)
+		},
+		"dict": func(values ...interface{}) (map[string]interface{}, error) {
+			if len(values)%2 != 0 {
+				return nil, errors.New("invalid dict call")
+			}
+			dict := make(map[string]interface{}, len(values)/2)
+			for i := 0; i < len(values); i += 2 {
+				key, ok := values[i].(string)
+				if !ok {
+					return nil, errors.New("dict keys must be strings")
+				}
+				dict[key] = values[i+1]
+			}
+			return dict, nil
+		},
+	}
+
+	m.Templates = template.New("").Funcs(fm)
+	err := filepath.Walk("./templates", func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			b, err := ioutil.ReadFile(path)
+			if err != nil {
+				return err
+			}
+
+			m.Templates, err = m.Templates.
+				New(strings.TrimPrefix(path, "templates/")).
+				Parse(string(b))
+			return err
+		}
+		return nil
+	})
+
+	return err
 }
-
-// Anonymous implements the Principal interface for unauthenticated
-// users and can be used as a fallback principal when none is set
-// in the current session.
-type Anonymous struct{}
-
-// Authenticated returns always false, because Anonymous users are not
-// authenticated.
-func (a Anonymous) Authenticated() bool { return false }
-
-// ID retunrs always the string `anonymous` as ID for unauthenticated
-// users.
-func (a Anonymous) ID() string { return "anonymous" }
-
-// HasAnyRole returns always false for any role, because Anonymous users
-// are not authenticated.
-func (a Anonymous) HasAnyRole(roles ...string) bool { return false }
 
 // BindingResult holds validation errors of the binding process from a HTML
 // form to a Go struct.
